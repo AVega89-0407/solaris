ef67da1be1181a105846dcc01d5fb985e908f11a
AVega89-0407 <andreavega89.av@gmail.com>
2025-12-03 16:24:40 +0100

    js fixes and styling

--- script.js
+++ script.js
@@ -1,74 +1,63 @@
+const x = document.querySelector(".x")
+
+const planetName = document.getElementById("planetName");
+const planetTitle = document.getElementById("planetTitle");
+const planetInfo = document.getElementById("planetInfo");
+const planetDetails = document.querySelector(".planet-details")
+
+planetDetails.style.display = 'none';
 
 
 let planets = [];
 
-// const button = document.getElementById("info").addEventListener("click", 
-// 
+//hämtar API-url och API-nyckeln
 async function loadPlanets() {
-
+ 
   let resp = await fetch('https://corsproxy.io/?https://4a6l0o1px9.execute-api.eu-north-1.amazonaws.com/bodies', {
     method: 'GET',
     headers: {'x-zocom': 'solaris-HipRojQEq5sRjt3s'}
 })
+
 let data = await resp.json();
 planets = data.bodies;
-console.log(planets);
+
+console.log(data);
 }
 
-const sunBtn = document.getElementById("0")
-const mercuryBtn = document.getElementById("1")
-const venusBtn = document.getElementById("2")
-const tellusBtn = document.getElementById("3")
-const marsBtn = document.getElementById("4")
-const jupiterBtn = document.getElementById("5")
-const saturnusBtn = document.getElementById("6")
-const uranusBtn = document.getElementById("7")
-const neptunusBtn = document.getElementById("8")
-const x = document.querySelector(".x")
-const modal = document.querySelector(".modal")
-const overlay = document.querySelector(".overlay")
+//funktion för att visa varje planet
+function showPlanet(planetNameToShow) {
+const planet = planets.find(p => p.name.toLowerCase() === planetNameToShow.toLowerCase());
 
-//funktion för att anropa overlay till en planet
-function open(e) {
-  const planetName = e.target.dataset.planet;
-    if (!planetName) return;
-
-  const planet = planets.find(p => p.name.toLowerCase() === planetName);
-    if (!planet) return;
-    
-  document.getElementById("planetName").innerText = planet.name;
-  document.getElementById("planetRubrik").innerText = planet.latinName;
-  document.getElementById("planetInfo").innerText = planet.desc;
-
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+if (!planet) {
+  planetDetails.style.display = 'none';
+  alert("Planet not found!");
+  return;
 }
 
-// sunBtn.addEventListener('click', open)
-// mercuryBtn.addEventListener('click', open)
-// venusBtn.addEventListener('click', open)
-// tellusBtn.addEventListener('click', open)
-// marsBtn.addEventListener('click', open)
-// jupiterBtn.addEventListener('click', open)
-// saturnusBtn.addEventListener('click', open)
-// uranusBtn.addEventListener('click', open)
-// neptunusBtn.addEventListener('click', open)
 
-document.addEventListener('click', open);
+//informationen som ska synas i overlayen
+planetName.innerText = planet.name;
+planetTitle.innerText = planet.latinName;
+planetInfo.innerText = planet.desc;
+
+planetDetails.style.display = 'block';
+} 
+
+//stänger overlayen
+x.addEventListener('click', (e) => {
+  e.preventDefault();
+  planetDetails.style.display = 'none';
+})
+  
+//Hämtar varje enskild planet när man klickar på en planet
+document.querySelectorAll("[data-planet]").forEach(planetElement => {
+
+  planetElement.addEventListener('click', () => {
+    const planet = planetElement.dataset.planet;
+    showPlanet(planet);
+  });
+
+});
+
+//laddar om sidan och hämtar API på nytt
 loadPlanets();
-
-//funktion för att stänga ner overlayen
-
-
-
-x.addEventListener('click', () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-
